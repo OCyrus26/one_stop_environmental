af0d12268b99649dc70a1eaae4a7d1ba0736fb1b
O'Cyrus JB <s1291542@monmouth.edu>
2021-05-21 13:10:46 -0400

    5/21

--- src/App.js
+++ src/App.js
@@ -6,11 +6,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Services from './components/Pages/Services';
 import Products from './components/Pages/Products';
 import SignUp from './components/Pages/SignUp';
-import LawnCare from './components/Pages/LawnCare';
-import PressureWashing from './components/Pages/PressureWashing';
-import MulchNeeds from './components/Pages/MulchNeeds';
-import PlantNeeds from './components/Pages/PlantNeeds';
-
+  /* "react-google-maps" : "^9.4.5", goes in package.json*/
 /* import {GoogleMap, withScriptjs, withGoogleMap} from "react-google-maps"; */
 /*
 function Map(){
@@ -30,10 +26,6 @@ function App() {
           <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
           <Route path='/sign-up' component={SignUp} />
-          <Route path='/lawn-care' component={LawnCare} />
-          <Route path='/heated-pressure-washing' component={PressureWashing} />
-          <Route path='/mulch-needs' component={MulchNeeds} />
-          <Route path='/plant-needs' component={PlantNeeds} />
         </Switch>
       </Router>
     </>
