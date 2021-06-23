81c76cd51e4d2dc48ca5233bc7e63322e0c2d8de
YosefDavidowitz <55250971+YosefDavidowitz@users.noreply.github.com>
2021-06-23 14:57:13 -0400

    6/23/21
    Fixed services section

--- src/components/AboutUsCards.js
+++ src/components/AboutUsCards.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import './Cards.css';
 import CardItem from './CardItem';
-import './SidePanel.css'
-import SidePanel from './SidePanel';
 
 function AboutUsCards() {
   return (
     <div className='cards'>
-      <h1>About Us </h1> 
+      
+      <h1>About Us </h1>
       <br/>
-      <h3>
-        Golden Wolf Landscape and Design specializes in the wide-ranging landscape design and installation of all your outdoor projects all over Hunterdon, Somerset, Warren, and Morris Counties. We have been servicing these areas for two years now, and we take pride in providing high-quality services to our clients. Our team has extensive experience in lawn maintenance and landscape designing, and we are equipped with the latest technology to provide you with exquisite landscape design.
-We understand the significance of delivering a design plan that is both geared to what customer wants and fit their budget as well. Our crew is trained to focus on the customers’ needs and wants first and then design their landscape accordingly. Your preferences are our utmost priority.
-Founder and owner Marcus Celiano takes his job with immense pride. Developing relationships with clients, training staff members, and building a company to serve the local community is what Marcus focuses on day in and day out. 
-       </h3>
-
-      <br/>
-      <br/>
+     
+      <p>Put a few sentences here</p>
+    
+    
       <h1>OUR MISSION</h1>
-      <h3>Our team of knowledge, experiences, and entrusted professionals is prepared to tackle any job that comes our way. Ranging from design to maintence, Golden Wolf Landscape & Design is the right choice for all of your landscaping needs. We do each job right the first time with high quality materials, exceeding industry standards practices, and making you, our client, our upmost priority. Our portfolio of completed projects is pictorial proof of our excellence and high-quality work. See what a difference the Golden Wolf way will make on your property!
-      </h3>
+      <p>Insert external links to organizations</p>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
@@ -38,7 +32,6 @@ Founder and owner Marcus Celiano takes his job with immense pride. Developing re
               text='⠀⠀⠀⠀⠀⠀⠀Name⠀⠀⠀⠀⠀⠀⠀'
               label='We have to put something in this blue box too'
             />
-
           </ul>
         </div>
       </div>
@@ -50,8 +43,8 @@ Founder and owner Marcus Celiano takes his job with immense pride. Developing re
           <ul className='cards__items'>
             <CardItem
               src='./assets/mt1.png'
-              text='Jerm on the right Jeff of the right'
-              label='Jeff and Jerm'
+              text='Name⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀'
+              label='We have to put something in this blue box too'
             />
            
           </ul>
--- src/components/Pages/Services.js
+++ src/components/Pages/Services.js
@@ -10,6 +10,109 @@ function Services() {
   return(
     <>
 
+
+
+<div class='lawn' id='lawn'>
+        {/* <h1>Lawn Care </h1> 
+          <br/>
+       <p>Proper maintenance and care are crucial to extending the life of your installed trees, plants, and  grass. We'll provide weekly/ monthly landscape and lawn maintenance programs to ensure that your property stays beautiful from season to season. Give us a call to learn more. 
+        <br/>
+          <br/> */}
+
+
+          <div class="link__container">
+            <h1>Lawn Care </h1> 
+            <br/>
+            <p>Proper maintenance and care are crucial to extending the life of your installed trees, plants, and  grass. We'll provide weekly/ monthly landscape and lawn maintenance programs to ensure that your property stays beautiful from season to season. Give us a call to learn more. 
+            </p>
+            <br/>
+            <h2>Our Work</h2>
+
+            <br/>
+            <br/>
+
+
+            <br/>
+
+            <a href="#home" id="website__link__one" class="website__link">Service 1</a>
+                
+            <br/>
+
+            <a href="" id="website__link__two" class="website__link">Service 2</a>
+
+            <br/>
+            
+            <img id="website__picture__one" 
+                src = "./assets/Image 1.png"
+                width= "400"
+                height= "400"/> 
+
+            <img id="website__picture__two" 
+                src = "./assets/Image 2.png"               
+                width= "400"
+                height= "400"/> 
+
+
+          
+
+          {/* <img id="website__picture__default" 
+          src = "https://lh3.googleusercontent.com/UrILvSFamwzofZ0hzAEFCKl4o-kSt8Ca3VAwcg5TL0K83pOYzVx2upITNM_OMjijGot8B2sZ7Vte8ignaI_GdFVNFw=w640-h400-e365-rj-sc0x00ffffff"
+          width= "480"
+          height= "270"/> 
+          <br/> */}
+
+          </div>
+
+
+
+
+
+
+
+       
+          {/* <h2> Our Work </h2>
+          <div className='cards__container'>
+          <div className='cards__wrapper'>
+            <ul className='cards__items'>
+             <CardItem
+                src='./assets/img1.jpeg'
+                text='Caption⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀'
+                label='Lawn Care'
+              />
+  
+              <CardItem
+                src='./assets/img1.jpeg'
+                text='Caption⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀'
+                label='Lawn Care' />
+                <CardItem
+                src='./assets/img1.jpeg'
+                text='Caption⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀'
+                label='Lawn Care' />
+              </ul>
+          </div>
+          </div> */}
+  
+        {/* </p> */}
+        
+        <p>Lawn Cutting</p>
+        <p>Landscape Detailing</p>
+        <p>Service 3</p>
+      </div>
+  
+
+      {/* <div class='washing' id='washing'>
+        <h1>Heated Power Washing</h1> 
+        
+          <br/>
+       <p>Proper maintenance and care are crucial to extending the life of your installed trees, plants, and  grass. We'll provide weekly/ monthly landscape and lawn maintenance programs to ensure that your property stays beautiful from season to season. Give us a call to learn more. 
+
+
+ */}
+
+
+
+
+
       <div class='lawn' id='lawn'>
         <h1>Lawn Care </h1> 
         
